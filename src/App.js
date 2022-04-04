c8b8f19704b0a6ab1a8ae4f355022ae3afd90a75
Mahbub-Sajon <sajon.mahbub@gmail.com>
2022-04-04 18:07:25 +0600

    router set

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Header from './component/Header/Header';
+import Home from './component/Home/Home'
+import Reviews from './component/Reviews/Reviews'
+import Dashboard from './component/Dashboard/Dashboard'
+import Blogs from './component/Blogs/Blogs'
+import About from './component/About/About'
+import NotFound from './component/NotFound/NotFound';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello !!!</h1>
-      <Header></Header>
+    <Routes>
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="/home" element={<Home/>}></Route>
+      <Route path="/reviews" element={<Reviews/>}></Route>
+      <Route path="/dashboard" element={<Dashboard/>}></Route>
+      <Route path="/blogs" element={<Blogs/>}></Route>
+      <Route path="/about" element={<About/>}></Route>
+      <Route path="*" element={<NotFound/>}></Route>
+
+    </Routes>
     </div>
   );
 }
--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Navbar from '../Navbar/Navbar';
 
 const Header = () => {
     return (
         <div>
             <h1>This is header</h1>
+            <Navbar></Navbar>
         </div>
     );
 };

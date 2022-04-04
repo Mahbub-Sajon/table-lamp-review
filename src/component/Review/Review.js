ef7979be2289548b5bfb011b38dee835ea1cb81a
Mahbub-Sajon <sajon.mahbub@gmail.com>
2022-04-04 23:58:56 +0600

    data updated

---
+++ src/component/Review/Review.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Review = (props) => {
+    const {name, img, ratings, opinion} = props.user;
+    return (
+        <div>
+            <h1>Name: {name}</h1>
+            <img src={img} alt="" />
+            <h4>Ratings: {ratings}</h4>
+            <p>Review of the customer: {opinion}</p>
+
+        </div>
+    );
+};
+
+export default Review;
--- src/component/Reviews/Reviews.js
+++ src/component/Reviews/Reviews.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Review from '../Review/Review';
+
 
 const Reviews = () => {
+    const [users, setUsers] =useState([]);
+    useEffect(() => {
+        fetch('data.json')
+        .then(res => res.json())
+        .then(data => setUsers(data));
+    },[])
     return (
         <div>
             <h1>This is reviews</h1>
+            {
+                users.map(user => <Review
+                key = {user.id}
+                user = {user}
+                
+                ></Review>)
+            }
         </div>
     );
 };
